4abeaddedfb276a54207a45139a2c1caaeebf43d
Admin <socorpkolathur@gmail.com>
2023-01-02 11:11:05 +0530

    02@lsm

---
+++ lib/Main_Page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Main_Page extends StatefulWidget {
+  const Main_Page({Key? key}) : super(key: key);
+
+  @override
+  State<Main_Page> createState() => _Main_PageState();
+}
+
+class _Main_PageState extends State<Main_Page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
